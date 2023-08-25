9acd15dd2a7777bd37adb278a9e8960e5f3970e4
Don Bhanuka <bhanukamax@gmail.com>
2023-08-25 22:26:39 +0530

    feat: step0 done

--- impls/ocaml2/step0_repl.ml
+++ impls/ocaml2/step0_repl.ml
@@ -1,1 +1,12 @@
-print_string "hello"
+let read x = x
+let eval x = x
+let print x = print_endline x
+
+let rec rep () =
+  print_string "user> ";
+  let input = read_line () in
+  input |> read |> eval |> print;
+  rep ()
+;;
+
+let _ = rep ()
