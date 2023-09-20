5bd949672b6e73e0021bfe93d641d60d14c1d04e
Don Bhanuka <bhanukamax@gmail.com>
2023-09-20 21:50:58 +0530

    step4: add built in comparison operators
    TEST RESULTS (for ../tests/step4_if_fn_do.mal):
        0: soft failing tests
      101: failing tests
       77: passing tests
      178: total tests

--- impls/ocaml.2/core.ml
+++ impls/ocaml.2/core.ml
@@ -4,14 +4,27 @@ open Printer
 open Env
 open Debug_printers
 
+let get_builtin symbol op env =
+  let _ =
+    Env.set
+      symbol
+      (MalFn
+         (fun a ->
+           match List.nth_opt a 0, List.nth_opt a 1 with
+           | Some (MalAtom (Number x)), Some (MalAtom (Number y)) ->
+             if op (int_of_string x) (int_of_string y)
+             then MalAtom True
+             else MalAtom False
+           | _ -> MalAtom False))
+      env
+  in
+  ()
+;;
+
 let setup_ns env =
-  Env.set
-    "="
-    (MalFn
-       (fun a ->
-         match List.nth_opt a 0, List.nth_opt a 1 with
-         | Some (MalAtom (Number x)), Some (MalAtom (Number y)) ->
-           if int_of_string x == int_of_string y then MalAtom True else MalAtom False
-         | _ -> MalAtom False))
-    env
+  get_builtin ">=" ( >= ) env;
+  get_builtin "<=" ( <= ) env;
+  get_builtin ">" ( > ) env;
+  get_builtin "<" ( < ) env;
+  get_builtin "=" ( == ) env
 ;;
