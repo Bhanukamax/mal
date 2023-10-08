f2389c387c2efb4848fa612726b8d9b16eda0311
Don Bhanuka <bhanukamax@gmail.com>
2023-10-08 19:03:31 +0530

    step0: added missing functions

--- impls/haskell.2/step0_repl.hs
+++ impls/haskell.2/step0_repl.hs
@@ -1,13 +1,23 @@
 module Main where
 import System.IO
 
+read' :: String -> String
+read' x = x
+
+eval' :: a -> a
+eval' x = x
+
+print' :: String -> IO ()
+print' x =
+    putStrLn x
+
 rep :: IO ()
 rep =
   do
     putStr "user> "
     hFlush stdout
     line <- getLine
-    putStrLn line
+    (print' . eval' . read' ) line
     rep
 
 main :: IO ()
